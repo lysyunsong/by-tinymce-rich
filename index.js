8513e6680a8a7fa2d198cbd90ed3f1e03d5496c6
liyunsong <764133664@qq.com>
2021-12-20 08:29:12 +0800

    修改引用错误

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import tinymce_rich from './src/components/tinymce_rich/index'
  
 import _Vue from 'vue'
 
-Main.install = Vue => {
-if (!Vue) {
-window.Vue = Vue = _Vue
-}
-Vue.component(Main.name, Main)
-}
+// Main.install = Vue => {
+// if (!Vue) {
+// window.Vue = Vue = _Vue
+// }
+// Vue.component(Main.name, Main)
+// }
+export { tinymce_rich };
 export default tinymce_rich;
